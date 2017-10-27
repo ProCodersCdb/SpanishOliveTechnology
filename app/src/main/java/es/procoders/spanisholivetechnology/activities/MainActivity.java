1b1223629af17c6f04a1e03adadee356349d118f
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-10-27 13:53:15 +0200

    Cambio

--- app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import es.procoders.spanisholivetechnology.R;
+import es.procoders.spanisholivetechnology.controllers.MainController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainController mController = new MainController();
     }
 
 
