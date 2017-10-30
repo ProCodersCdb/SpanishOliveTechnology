732e30d5d06fd2240c9c8965a0e0cf5e05607a52
juanfranem <31940210+juanfranem@users.noreply.github.com>
2017-10-30 13:38:51 +0100

    Fragments realizados y controlador de biomasa

--- app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentDetails.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentDetails.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import es.procoders.spanisholivetechnology.R;
+import es.procoders.spanisholivetechnology.controllers.BiomasaController;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BiomasaFragmentDetails extends Fragment {
+
+    BiomasaController controller;
+    private int position;
 
 
     public BiomasaFragmentDetails() {
@@ -24,8 +28,45 @@ public class BiomasaFragmentDetails extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_biomasa_fragment_details, container, false);
+        controller = BiomasaController.getInstance();
+        position = controller.getPosition();
+        inflate(position);
+        View rootView = inflater.inflate(inflate(position), container, false);
+
         return rootView;
     }
+
+
+    public int inflate(int i){
+        int layout;
+        switch (i){
+            case 0:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 1:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 2:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 3:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 4:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 5:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            case 6:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+            default:
+                layout = R.layout.fragment_biomasa_fragment_details;
+                break;
+        }
+        return layout;
+    }
+
 
 }
--- app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentMain.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentMain.java
@@ -3,26 +3,28 @@ package es.procoders.spanisholivetechnology.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import es.procoders.spanisholivetechnology.R;
 import es.procoders.spanisholivetechnology.adapters.ListViewAdapter;
+import es.procoders.spanisholivetechnology.controllers.BiomasaController;
 import es.procoders.spanisholivetechnology.questions.BiomasaQuestions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BiomasaFragmentMain extends Fragment {
+public class BiomasaFragmentMain extends Fragment implements AdapterView.OnItemClickListener {
 
     ListView lv;
     BiomasaQuestions bq;
     BaseAdapter adapter;
-
-    //array bioPreguntas;
+    BiomasaController controller;
 
 
     public BiomasaFragmentMain() {
@@ -36,6 +38,8 @@ public class BiomasaFragmentMain extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_biomasa_fragment_main, container, false);
         // Inflate the layout for this fragment
         initViews(rootView);
+        controller = BiomasaController.getInstance();
+
 
         return rootView;
 
@@ -46,6 +50,17 @@ public class BiomasaFragmentMain extends Fragment {
         lv = (ListView) view.findViewById(R.id.listView_mainFragment);
         adapter = new ListViewAdapter(view, bq.getBioPreguntas());
         lv.setAdapter(adapter);
+
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Fragment nuevoFragmento = new BiomasaFragmentDetails();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        controller.setPosition(i);
+        transaction.replace(R.id.fragment_activityBiomasa, nuevoFragmento);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
 }
