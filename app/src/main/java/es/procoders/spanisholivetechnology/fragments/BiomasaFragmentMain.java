d400ee20315f27cdb06c40fd490c5fd7acbbb252
juanfranem <31940210+juanfranem@users.noreply.github.com>
2017-10-30 10:09:13 +0100

    Adaptador y MainList

--- app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentMain.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentMain.java
@@ -6,13 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import es.procoders.spanisholivetechnology.R;
+import es.procoders.spanisholivetechnology.adapters.ListViewAdapter;
+import es.procoders.spanisholivetechnology.questions.BiomasaQuestions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BiomasaFragmentMain extends Fragment {
+
+    ListView lv;
+    BiomasaQuestions bq;
+    BaseAdapter adapter;
+
+    //array bioPreguntas;
 
 
     public BiomasaFragmentMain() {
@@ -25,7 +35,17 @@ public class BiomasaFragmentMain extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_biomasa_fragment_main, container, false);
         // Inflate the layout for this fragment
+        initViews(rootView);
+
         return rootView;
+
+    }
+
+    private void initViews(View view) {
+        bq = new BiomasaQuestions();
+        lv = (ListView) view.findViewById(R.id.listView_mainFragment);
+        adapter = new ListViewAdapter(view, bq.getBioPreguntas());
+        lv.setAdapter(adapter);
     }
 
 }
