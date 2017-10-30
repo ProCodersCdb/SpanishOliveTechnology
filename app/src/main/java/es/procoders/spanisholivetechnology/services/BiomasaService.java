303538cf82187bc27017623501c3df9892dbe76e
Bardo1992 <32056513+Bardo1992@users.noreply.github.com>
2017-10-30 12:51:08 +0100

    Primer trabajo sobre Services

--- app/src/main/java/es/procoders/spanisholivetechnology/services/BiomasaService.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/services/BiomasaService.java
@@ -1,4 +1,23 @@
 package es.procoders.spanisholivetechnology.services;
 
+import es.procoders.spanisholivetechnology.activities.BiomasaActivity;
+
 public class BiomasaService {
+
+    private BiomasaActivity = new BiomasaActivity();
+
+    public Boolean CheckAnswer() {
+
+        /* con esta linea de codigo nos cubrimos de que las preguntas que son de obligatoria
+        cumplimentacion no queden sin rellenar. Una vez que quedan rellenas, se confirma. En caso
+        de no cumplirse la condicion devuelve un false */
+
+        if (BiomasaActivity.bioQ2 != null && BiomasaActivity.bioQ3 != null && BiomasaActivity.bioQ5 != null && BiomasaActivity.bioQ6 != null) {
+
+            return true;
+        }
+
+        return false;
+
+    }
 }
