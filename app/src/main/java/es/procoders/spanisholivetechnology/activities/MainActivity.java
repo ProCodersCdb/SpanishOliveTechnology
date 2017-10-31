2c22b99c2724d62d901598e9d05bc0af019a21d0
saitoh00 <afernangar@gmail.com>
2017-10-31 11:57:53 +0100

    Añadido MainController y activity_main.xml (añadido boton)

--- app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
@@ -6,13 +6,12 @@ import android.os.Bundle;
 import es.procoders.spanisholivetechnology.R;
 import es.procoders.spanisholivetechnology.controllers.MainController;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         MainController mController = new MainController();
     }
 
