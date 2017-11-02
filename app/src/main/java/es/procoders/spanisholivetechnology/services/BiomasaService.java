a5b738cad93f72bc8dff63e4aee1cc7caa2168e3
José María Raya Bejarano <joserayab@gmail.com>
2017-11-02 09:56:10 +0100

    Merge branch 'Testing' of https://github.com/ProCodersCdb/SpanishOliveTechnology into JoseTest
    # Conflicts:
    #	app/src/main/java/es/procoders/spanisholivetechnology/adapters/ListViewAdapter.java
    #	app/src/main/java/es/procoders/spanisholivetechnology/controllers/BiomasaController.java
    #	app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentDetails.java
    #	app/src/main/java/es/procoders/spanisholivetechnology/fragments/BiomasaFragmentMain.java
    #	app/src/main/java/es/procoders/spanisholivetechnology/services/BiomasaService.java

--- app/src/main/java/es/procoders/spanisholivetechnology/services/BiomasaService.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/services/BiomasaService.java
@@ -5,17 +5,23 @@ import es.procoders.spanisholivetechnology.beans.BiomasaBean;
 
 public class BiomasaService {
 
-    public Boolean CheckAnswer(BiomasaBean datosBio) {
+//    public BussinessRule [] rules = new BussinessRule[1];
+
+    public Boolean isReady(BiomasaBean datosBio) {
 
         /* con esta linea de codigo nos cubrimos de que las preguntas que son de obligatoria
         cumplimentacion no queden sin rellenar. Una vez que quedan rellenas, se confirma. En caso
         de no cumplirse la condicion devuelve un false */
 
+        Boolean retVal = false;
+
         if (datosBio.getBioQ2().toString() != null && datosBio.getBioQ3().toString() != null && datosBio.getBioQ5().toString() != null && datosBio.getBioQ6().toString() != null) {
 
-            return true;
+            retVal = true;
         }
 
-        return false;
+        return retVal;
     }
+
+//    public BussinessRule getRule ()
 }
