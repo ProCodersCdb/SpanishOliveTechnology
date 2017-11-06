9d621792d4082b708e7c09bb1b1746fe47f6f6a1
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-11-06 10:51:51 +0100

    Subida de logotipo y colores

---
+++ app/src/main/java/es/procoders/spanisholivetechnology/activities/LanguageActivity.java
@@ -0,0 +1,15 @@
+package es.procoders.spanisholivetechnology.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import es.procoders.spanisholivetechnology.R;
+
+public class LanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language);
+    }
+}
