ea9a47d1286a6b382f8e39b1c0b9d60580d6a4e4
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-11-09 12:31:56 +0100

    Ajustes gráficos

--- app/src/main/java/es/procoders/spanisholivetechnology/questions/BiomasaQuestions.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/questions/BiomasaQuestions.java
@@ -1,12 +1,9 @@
 package es.procoders.spanisholivetechnology.questions;
 
 
-import android.app.Activity;
-import android.app.VoiceInteractor;
 import android.content.Context;
 import android.content.res.Resources;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import es.procoders.spanisholivetechnology.R;
@@ -49,14 +46,14 @@ public class BiomasaQuestions {
          */
 
     public ArrayList<Options> rellenarBioPreguntas() {
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails1), false, R.layout.bio_details_1));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails2), true, R.layout.bio_details_2));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails3), true, R.layout.bio_details_3));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails4), false, R.layout.bio_details_4));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails5), true, R.layout.bio_details_5));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails6), true, R.layout.bio_details_6));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails7), false, R.layout.bio_details_7));
-        bioPreguntas.add(new Options(res.getString(R.string.txtDetails8), false, R.layout.bio_details_8));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle1), false, R.layout.bio_details_1));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle2), true, R.layout.bio_details_2));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle3), true, R.layout.bio_details_3));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle4), false, R.layout.bio_details_4));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle5), true, R.layout.bio_details_5));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle6), true, R.layout.bio_details_6));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle7), false, R.layout.bio_details_7));
+        bioPreguntas.add(new Options(res.getString(R.string.bioQTittle8), false, R.layout.bio_details_8));
         return bioPreguntas;
 
     }
