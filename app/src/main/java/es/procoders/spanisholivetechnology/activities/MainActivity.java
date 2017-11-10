2bc5bf954b562ab60d91696ac747e538f4df347d
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-11-10 12:45:31 +0100

    Menu lateral implementado

--- app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/activities/MainActivity.java
@@ -1,131 +1,39 @@
 package es.procoders.spanisholivetechnology.activities;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import es.procoders.spanisholivetechnology.R;
-import es.procoders.spanisholivetechnology.controllers.BiomasaController;
 import es.procoders.spanisholivetechnology.controllers.MainController;
 
-/**
- * @author Procoders
- * @since API 21
- * @version 1.0
- */
 public class MainActivity extends AppCompatActivity  {
 
-    /**
-     *
-     * @param savedInstanceState hace un guardado de la información recabada en cada una de las
-     *                           preguntas
-     */
-    private Toolbar toolbar;
-    private AppBarLayout appBarLayout;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    MainController mController;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mController = new MainController(this);
 
-        MainController mController = new MainController(this);
+        mController.setToolBar(this);
 
-        setToolBar();
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView =(NavigationView) findViewById(R.id.navview);
-        //setFragmentByDefault();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                boolean fragmentTransaction = false;
-                Fragment fragment= null;
-
-
-                switch (item.getItemId()){
-                    case R.id.menu_plantacion:
-                       // fragment=new Plantacion();
-                        fragmentTransaction = true;
-
-
-                        break;
-                    case R.id.menu_almanzara:
-                      //  fragment=new Almanzara();
-                        fragmentTransaction = true;
-
-                        break;
-                    case R.id.menu_fabrica:
-
-
-
-                        break;
-
-                    case R.id.menu_aceite:
-
-
-                        break;
-
-                    case R.id.menu_aceituna:
-
-                        break;
-                    case R.id.menu_biomasa:
-                        startActivity(new Intent(getApplicationContext(),BiomasaActivity.class));
-
-                        break;
-                }
-                if(fragmentTransaction){
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.content_frame, fragment)
-                            .commit();
-                    item.setChecked(true);
-                    changeFragment(fragment, item);
-                    drawerLayout.closeDrawers();
-
-                }
-
-                return true;
-            }
-        });
-    }
-
-    /*private void setFragmentByDefault() {
-        changeFragment(new BiomasaActivity(),navigationView.getMenu().getItem(0));
-    }*/
-
-    private void changeFragment(Fragment fragment, MenuItem item) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
-        item.setChecked(true);
-        getSupportActionBar().setTitle(item.getTitle());
-
+        mController.loadMenu(this);
 
     }
 
-    private void setToolBar() {
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()){
             case android.R.id.home:
                 //abrir menú lateral
-                drawerLayout.openDrawer(GravityCompat.START);
+                mController.openDrawer();
                 return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/es/procoders/spanisholivetechnology/controllers/MainController.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/controllers/MainController.java
@@ -2,8 +2,18 @@ package es.procoders.spanisholivetechnology.controllers;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import es.procoders.spanisholivetechnology.R;
 import es.procoders.spanisholivetechnology.activities.BiomasaActivity;
@@ -14,9 +24,15 @@ import es.procoders.spanisholivetechnology.activities.BiomasaActivity;
  * @version 1.0
  */
 
-public class MainController implements View.OnClickListener {
+public class MainController {
+
+    private static DrawerLayout drawerLayout;
     private Activity activity;
-    Button btn ;
+    private Button btn ;
+    private Toolbar toolbar;
+    private static AppBarLayout appBarLayout;
+    private static NavigationView navigationView;
+    private View view;
 
     /**
      * @param activity el controlador recibe este parámetro y trabaja sobre él
@@ -29,10 +45,96 @@ public class MainController implements View.OnClickListener {
         //btn.setOnClickListener(this);
     }
 
+    // ######################## GESTION DE MENU LATERAL ################################
 
-    @Override
-    public void onClick(View view) {
-        Intent intent=new Intent(activity, BiomasaActivity.class);
-        activity.startActivity(intent);
+
+    public static void changeFragment(Fragment fragment, MenuItem item,Activity v) {
+        ((AppCompatActivity)v).getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        item.setChecked(true);
+        ((AppCompatActivity)v).getSupportActionBar().setTitle(item.getTitle());
+
+
+    }
+
+    public static void setToolBar(Activity v) {
+        Toolbar toolbar=(Toolbar) v.findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        ((AppCompatActivity)v).setSupportActionBar(toolbar);
+        ((AppCompatActivity)v).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        ((AppCompatActivity)v).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public static void loadMenu(final Activity act){
+
+        drawerLayout = act.findViewById(R.id.drawer_layout);
+        navigationView =(NavigationView) act.findViewById(R.id.navview);
+        //setFragmentByDefault();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                boolean fragmentTransaction = false;
+                Fragment fragment= null;
+
+
+                switch (item.getItemId()){
+                    case R.id.inicio:
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.menu_plantacion:
+                        // fragment=new Plantacion();
+                        fragmentTransaction = true;
+
+
+                        break;
+                    case R.id.menu_almanzara:
+                        //  fragment=new Almanzara();
+                        fragmentTransaction = true;
+
+                        break;
+                    case R.id.menu_fabrica:
+
+
+                        break;
+
+                    case R.id.menu_aceite:
+
+
+                        break;
+
+                    case R.id.menu_aceituna:
+
+                        break;
+                    case R.id.menu_biomasa:
+                        act.startActivity(new Intent(act.getApplicationContext(),BiomasaActivity.class));
+
+                        break;
+                }
+                if(fragmentTransaction){
+                    ((AppCompatActivity)act).getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.content_frame, fragment)
+                            .commit();
+                    item.setChecked(true);
+                    changeFragment(fragment, item, new Activity());
+                    drawerLayout.closeDrawers();
+
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    public static void openDrawer(){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void toast(View v){
+        Toast.makeText(v.getContext(), "No se puede enviar la petición. Formulario no relleno.", Toast.LENGTH_LONG).show();
     }
 }
