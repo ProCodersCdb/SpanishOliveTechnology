3c6ddfadfaa47d3f786e8c4456c432afa889d1ff
juanfranem <bjfem@hotmail.com>
2017-11-14 01:22:23 +0100

    Usuario login - Modificar

--- app/src/main/java/es/procoders/spanisholivetechnology/activities/UserActivity.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/activities/UserActivity.java
@@ -6,59 +6,122 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import org.json.JSONObject;
+
+import java.util.Objects;
 
 import es.procoders.spanisholivetechnology.R;
 import es.procoders.spanisholivetechnology.beans.Usuario;
 import es.procoders.spanisholivetechnology.controllers.GeneralSingleton;
 
-public class UserActivity extends AppCompatActivity {
-    EditText name, phone, email;
-    Button save;
+public class UserActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText usuario, password, repassword, email;
+    Button login, register;
+    Switch nuevouser;
+    CheckBox saveLog;
+     SharedPreferences prefs;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        final SharedPreferences prefs = getSharedPreferences("usuario", Context.MODE_PRIVATE);
+        prefs = getSharedPreferences("usuario", Context.MODE_PRIVATE);
         checkExist(prefs);
-        name = findViewById(R.id.user_name);
-        phone = findViewById(R.id.user_phone);
-        email = findViewById(R.id.user_email);
-        save = findViewById(R.id.btn_user);
-        phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
-        save.setOnClickListener(new View.OnClickListener() {
+        usuario = findViewById(R.id.user_email);
+        password = findViewById(R.id.user_pass);
+        repassword = findViewById(R.id.user_pass2);
+        login = findViewById(R.id.btn_user);
+        register = findViewById(R.id.btn_register);
+        nuevouser = findViewById(R.id.nuevousuario);
+        saveLog = findViewById(R.id.saveCheck);
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
+        repassword.setVisibility(View.GONE);
+
+        nuevouser.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                String nombre = name.getText().toString();
-                String tele = phone.getText().toString();
-                String mail = email.getText().toString();
-                Usuario user = new Usuario(nombre, tele, mail);
-                GeneralSingleton.getInstance().setUser(user);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("nombre", nombre);
-                editor.putString("telefono", tele);
-                editor.putString("mail", mail);
-                editor.commit();
-                Intent inte = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(inte);
-                finish();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    repassword.setVisibility(View.VISIBLE);
+                    register.setVisibility(View.VISIBLE);
+                    login.setVisibility(View.GONE);
+                }else{
+                    repassword.setVisibility(View.GONE);
+                    register.setVisibility(View.GONE);
+                    login.setVisibility(View.VISIBLE);
+                }
             }
         });
+
     }
 
     private void checkExist(SharedPreferences prefs) {
-        if (prefs.getString("nombre", null)!= null){
-            Usuario user =new Usuario(prefs.getString("nombre", null), prefs.getString("telefono", null), prefs.getString("mail", null));
+        if (prefs.getString("email", null)!= null){
+            Usuario user =new Usuario();
+            user.setPassword(prefs.getString("password", null));
+            user.setEmail(prefs.getString("email", null));
             GeneralSingleton.getInstance().setUser(user);
             Intent inte = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(inte);
             finish();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btn_user:
+                if (!TextUtils.isEmpty(usuario.getText()) && !TextUtils.isEmpty(password.getText())){
+                    if (saveLog.isChecked()){
+                        guardarDatos(prefs);
+                        loginto();
+                    }else{
+                        loginto();
+                    }
+                }
+                break;
+            case R.id.btn_register:
+                if (!TextUtils.isEmpty(usuario.getText()) && Objects.equals(password.getText(), repassword.getText()) && !TextUtils.isEmpty(password.getText())){
+                    if (saveLog.isChecked()){
+                        guardarDatos(prefs);
+                        //saveinDB();
+                        loginto();
+                    }else{
+                        loginto();
+                    }
+                }
+                break;
+        }
+    }
+
+    private void loginto() {
+        Intent inte = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(inte);
+        finish();
+    }
+
+    private void guardarDatos(SharedPreferences prefs) {
+        String nombre="";
+        String pass = password.getText().toString();
+        String mail = email.getText().toString();
+        Usuario user = new Usuario();
+        user.setEmail(mail);
+        user.setPassword(pass);
+        GeneralSingleton.getInstance().setUser(user);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("email", mail);
+        editor.putString("password", pass);
+        editor.commit();
+
     }
 }
--- app/src/main/java/es/procoders/spanisholivetechnology/beans/Usuario.java
+++ app/src/main/java/es/procoders/spanisholivetechnology/beans/Usuario.java
@@ -10,11 +10,39 @@ public class Usuario implements Serializable{
 
     private String email;
     private String nombre;
-    private String phone;
+    private String password;
 
-    public Usuario(String email, String nombre, String phone) {
+    public Usuario(String email, String nombre, String password) {
         this.email = email;
         this.nombre = nombre;
-        this.phone = phone;
+        this.password = password;
+    }
+
+    public Usuario() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
